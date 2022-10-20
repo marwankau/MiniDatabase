cda44949e0a4342efec3146b3c6487727b2286c7
Basem <basem.bahattab.bm@gmail.com>
2022-10-20 09:04:46 +0300

    add data.db writer

--- src/App.java
+++ src/App.java
@@ -1,12 +1,25 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv", StandardCharsets.UTF_8))) {
+        File f = new File("data/data.db");
+        BufferedWriter writer = null;
+        try {
+            f.createNewFile();
+            writer = new BufferedWriter(new FileWriter(f));
+        } catch (IOException e1) {
+            System.err.println(e1.getMessage());
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
 
 
@@ -22,9 +35,45 @@ public class App {
 
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
 
+                String yearString = "";
+                yearString += year;
+                writeFixedString(yearString, writer, 5);
+                
+
+                writeFixedString(industryAggregation, writer, 10);
+                writeFixedString(industryCode, writer, 8);
+                writeFixedString(industryName, writer, 50);
+                
+                String valueString = "";
+                valueString += value;
+                writeFixedString(valueString, writer, 8);
+                
+                writer.write("\n");
             }
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
         }
+    }
+    private static void writeFixedString(String name, BufferedWriter bf, int length) throws IOException {
+        if (name.length() > length) {
+            name = name.substring(0, length);
+        }
+        char[] b = new char[length];
+        for (int i = 0; i < length; i++) {
+            if (i < name.length()) {
+                b[i] = name.charAt(i);
+            } else {
+                b[i] = ' ';
+            }
+            
+        }
+        bf.write(b);
     }
 }
