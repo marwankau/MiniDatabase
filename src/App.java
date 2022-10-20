60e4a93ea972a191b36440093ea8bc87a6ed83f0
moath alsolami <m-m-s-02@hotmail.com>
2022-10-20 09:12:40 +0300

    1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -11,19 +13,24 @@ import javax.xml.catalog.Catalog;
 
 public class App {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv", StandardCharsets.UTF_8))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
-
-
+            FileOutputStream fot =new FileOutputStream("data.db");
+            DataOutputStream dot = new DataOutputStream(fot);
             while ((line = reader.readLine()) != null) {
                 Scanner scanner = new Scanner(line);
                 scanner.useDelimiter(",");
 
                 int year = scanner.nextInt();
+                dot.write(year);
                 String industryAggregation = scanner.next();
+                dot.writeUTF(industryAggregation);
                 String industryCode = scanner.next();
+                dot.writeUTF(industryCode);
                 String industryName = scanner.next();
+                dot.writeUTF(industryName);
                 double value = scanner.nextDouble();
+                dot.writeDouble(value);
 
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
 
@@ -32,4 +39,5 @@ public class App {
             System.err.println(e.getMessage());
         }
     }
+    
 }
