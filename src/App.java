17e866c22fed320d407fb808e48b633b176d5a8b
Abdulaziz Alghamdi <aalghamdi2952@stu.kau.edu.sa>
2022-10-20 20:05:19 +0300

    mini data

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
@@ -10,26 +15,55 @@ import java.util.Scanner;
 import javax.xml.catalog.Catalog;
 
 public class App {
-    public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv", StandardCharsets.UTF_8))) {
-            String line = reader.readLine(); // skip column title (first line)
+    public static void main(String[] args) throws FileNotFoundException {
+       
+        FileOutputStream fos = new FileOutputStream("data/Data.db");
+        DataOutputStream dos = new DataOutputStream(fos);
+        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
+    
+                String line = reader.readLine();
 
-
-            while ((line = reader.readLine()) != null) {
-                Scanner scanner = new Scanner(line);
-                scanner.useDelimiter(",");
-
+                while ((line = reader.readLine()) != null) {
+                    Scanner scanner = new Scanner(line);
+                    scanner.useDelimiter(",");
+    
                 int year = scanner.nextInt();
                 String industryAggregation = scanner.next();
                 String industryCode = scanner.next();
                 String industryName = scanner.next();
                 double value = scanner.nextDouble();
+                dos.writeInt(year);
+                byte[] Getfixed = new byte[10];
+                writeFixStr(industryAggregation, Getfixed,8);
+                dos.write(Getfixed);
+
+                Getfixed = new byte[8];
+                writeFixStr(industryCode, Getfixed, 8);
+                dos.write(Getfixed);
+                Getfixed = new byte[50];
+                writeFixStr(industryName, Getfixed, 50);
+                dos.write(Getfixed);
+
+                dos.writeDouble(value);
 
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
 
             }
+            reader.close();
+            dos.close();
         } catch (IOException e) {
             System.err.println(e.getMessage());
+        }
+    }
+    private static void writeFixStr(String st, byte[] len, int length) throws IOException{
+        for(int i = 0; i < length; i++){
+            if (st.length() <= i){
+                len[i]= 0;
+
+
+            }else {
+                len[i] =(byte)st.charAt(i);
+            }
         }
     }
 }
