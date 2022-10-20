47a80adeaa4c7e286107fa8077b4597a14c79889
Mohammed Akif <77313110+iMoeDev@users.noreply.github.com>
2022-10-20 13:42:23 +0300

    miniDatabase Updated

--- src/App.java
+++ src/App.java
@@ -8,7 +8,9 @@ public class App {
         try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
 
-
+            FileOutputStream is = new FileOutputStream("data/source.db");
+            DataOutputStream dos = new DataOutputStream(is);
+            
             while ((line = reader.readLine()) != null) {
                 Scanner scanner = new Scanner(line);
                 scanner.useDelimiter(",");
@@ -18,6 +20,12 @@ public class App {
                 String industryCode = scanner.next();
                 String industryName = scanner.next();
                 double value = scanner.nextDouble();
+                
+                dos.writeInt(year);
+                dos.write(industryAggregation.getBytes());
+                dos.write(industryCode.getBytes());
+                dos.write(industryName.getBytes());
+                dos.writeDouble(value);
 
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
 
