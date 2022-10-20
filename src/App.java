ab61d194ff090d7b44fbcdce7069592ed5555f3f
MajeedReact <mjalsalhi@gmail.com>
2022-10-20 11:49:31 +0300

    writing binary

--- src/App.java
+++ src/App.java
@@ -1,22 +1,14 @@
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-import javax.xml.catalog.Catalog;
 
 public class App {
     public static void main(String[] args) throws IOException {
         RandomAccessFile raf = new RandomAccessFile("data/data.db", "rw");
-        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv", StandardCharsets.UTF_8))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
 
             while ((line = reader.readLine()) != null) {
@@ -29,17 +21,17 @@ public class App {
                 String industryCode = scanner.next();
                 String industryName = scanner.next();
                 double value = scanner.nextDouble();
-
+                
+                
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode,
                         industryName, value);
-
+                
                 // writing binary
                 raf.writeInt(year);
                 convertString2Byte(industryAggregation, 10, raf);
                 convertString2Byte(industryCode, 8, raf);
                 convertString2Byte(industryName, 50, raf);
                 raf.writeDouble(value);
-
             }
 
         } catch (IOException e) {
@@ -48,16 +40,15 @@ public class App {
     }
 
     public static void convertString2Byte(String string, int length, RandomAccessFile raf) throws IOException {
+        System.out.println(string);
         byte[] data = new byte[length];
         for (int i = 0; i < data.length; i++) {
-            if (string.length() <= i)
-                break;
-
-            data[i] = (byte) string.charAt(i);
-
+            if (i < string.length() ){
+                data[i] = (byte) string.charAt(i);
+            }
+                data[i] = 0;
         }
         raf.write(data);
 
     }
-
 }
