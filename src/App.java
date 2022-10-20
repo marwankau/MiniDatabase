6f305e5c3d783aee92c71e88f5ae97c4ed4e2b96
Abdulrahman Nahfawi vmware <nahfawi_abdelrahman49@yahoo.com>
2022-10-20 09:47:58 +0300

    x

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
-
 
             while ((line = reader.readLine()) != null) {
                 Scanner scanner = new Scanner(line);
@@ -19,11 +21,40 @@ public class App {
                 String industryName = scanner.next();
                 double value = scanner.nextDouble();
 
-                System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
+                System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode,
+                        industryName, value);
 
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+
+        
+        FileOutputStream fos = new FileOutputStream("data/data.db");
+
+        fos.write(byteConverter(null, null, null, 0));
+
+        
+
+    
+
+    }
+
+    public static byte [] byteConverter(String industryAgg, String industryCode, String industryName, double value){
+      industryAgg.getBytes();
+      industryCode.getBytes();
+      industryName.getBytes();
+
+      byte [] ba = new byte[30];
+
+      return ba;
+     
+
+
+
+
+        
+
+
     }
 }
