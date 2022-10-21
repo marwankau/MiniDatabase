52c55342fbe3695b4129342810a1fbecaa7335fe
Ziyad Alqahtani <zmdsaq@gmail.com.com>
2022-10-21 07:03:07 +0300

    Mini Database Acivity 4 Updated

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -7,7 +9,9 @@ public class App {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
-
+           
+            FileOutputStream fos = new FileOutputStream("data/data.db");
+            DataOutputStream dos = new DataOutputStream(fos);
 
             while ((line = reader.readLine()) != null) {
                 Scanner scanner = new Scanner(line);
@@ -19,11 +23,37 @@ public class App {
                 String industryName = scanner.next();
                 double value = scanner.nextDouble();
 
+
+                dos.writeInt(year);
+                dos.write(FixedString (industryAggregation.getBytes(),10));
+                dos.write(FixedString (industryCode.getBytes(),8));
+                dos.write(FixedString (industryName.getBytes(),50));
+                dos.writeDouble(value);
+
+
+
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
 
             }
+            
+            dos.close();
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
-}
+        public static byte[] FixedString (byte[] text, int len){
+            byte[] arr = new byte[len];
+    
+            for (int i = 0; i < len; i++) {
+                if(i < text.length){
+                    arr[i] = text[i];
+                }else{
+                    arr[i] = ' ';
+                }
+            }
+    
+            return arr;
+        }
+    }
+
