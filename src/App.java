59e7bca945e96d3fbcf9dfbe0514a96562ac5053
abdullahsaad2000 <harthi1127@gmail.com>
2022-10-23 21:29:59 +0300

    Submit late because I was absent thursday

--- src/App.java
+++ src/App.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new FileReader("data/source.csv"))) {
             String line = reader.readLine(); // skip column title (first line)
-
+            RandomAccessFile raf = new RandomAccessFile("data/data.db", "rw");
 
             while ((line = reader.readLine()) != null) {
                 Scanner scanner = new Scanner(line);
@@ -19,11 +20,35 @@ public class App {
                 String industryName = scanner.next();
                 double value = scanner.nextDouble();
 
+               raf.write(year);
+                raf.write(fillString(industryAggregation, 10));
+                raf.write(fillString(industryCode, 8));
+                raf.write(fillString(industryName, 10));
+                raf.writeDouble(value);
+
                 System.out.printf("%5d %-10s %-10s %-50s %10.2f\n", year, industryAggregation, industryCode, industryName, value);
 
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+
     }
+    private static byte[] fillString(String str, int len){
+        byte Byte[]= new byte[len];
+        
+        for(int i= 0; i < len ; i++){
+            if (i < str.length()) {
+                Byte [i] = (byte) str.charAt(i);
+            } else {
+                return Byte;
+            }
+        }
+        return Byte;
+            
+       
+         }
+
+
+
 }
